f44b7348931ff8aa8dadf7a54fa4edf9dae9107d
Ernesto Freyre <ernesto@bettercompany.co>
2017-05-11 10:45:42 -0700

    Add babel to transpile and commit the build using async to generators

---
+++ dist/index.js
@@ -0,0 +1,27 @@
+const { getParamsAndQuery } = require('../utils');
+
+const METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'];
+
+const methodFn = method => (path, handler) => {
+  if (!path) throw new Error('You need to set a valid path');
+  if (!handler) throw new Error('You need to set a valid handler');
+
+  return (req, res) => {
+    const { params, query } = getParamsAndQuery(path, req.url);
+
+    if (params && req.method === method) {
+      return handler(Object.assign(req, { params, query }), res);
+    }
+  };
+};
+
+exports.router = (...funcs) => async (req, res) => {
+  for (const fn of funcs) {
+    const result = await fn(req, res);
+    if (result || res.headersSent) return result;
+  }
+};
+
+METHODS.forEach(method => {
+  exports[method === 'DELETE' ? 'del' : method.toLowerCase()] = methodFn(method);
+});
