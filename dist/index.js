d2532dafbbe3a8b394d3b545b8790913f73ebaef
Ernesto Freyre <ernesto@bettercompany.co>
2017-05-11 10:51:56 -0700

    Added .babelrc, rebuilt

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,5 @@
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 const { getParamsAndQuery } = require('../utils');
 
 const METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'];
@@ -15,12 +17,18 @@ const methodFn = method => (path, handler) => {
   };
 };
 
-exports.router = (...funcs) => async (req, res) => {
-  for (const fn of funcs) {
-    const result = await fn(req, res);
-    if (result || res.headersSent) return result;
-  }
-};
+exports.router = (...funcs) => (() => {
+  var _ref = _asyncToGenerator(function* (req, res) {
+    for (const fn of funcs) {
+      const result = yield fn(req, res);
+      if (result || res.headersSent) return result;
+    }
+  });
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+})();
 
 METHODS.forEach(method => {
   exports[method === 'DELETE' ? 'del' : method.toLowerCase()] = methodFn(method);
