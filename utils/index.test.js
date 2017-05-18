4b449fbf51d1dcc1a6494e977c17ff7056c79ce5
Michael Trotter <spicydonuts@gmail.com>
2017-05-17 22:02:00 -0600

    Avoid rebuilding UrlPattern instances

--- utils/index.test.js
+++ utils/index.test.js
@@ -1,4 +1,5 @@
 const test = require('ava')
+const UrlPattern = require('url-pattern')
 
 const { getParamsAndQuery } = require('./')
 
@@ -7,6 +8,16 @@ test('getParamsAndQuery()', t => {
   const url = '/hello/world?id=0'
 
   const { params, query } = getParamsAndQuery(path, url)
+
+  t.deepEqual(params, { msg: 'world' })
+  t.deepEqual(query, { id: '0' })
+})
+
+test('getParamsAndQuery() with UrlPattern', t => {
+  const route = new UrlPattern('/hello/:msg')
+  const url = '/hello/world?id=0'
+
+  const { params, query } = getParamsAndQuery(route, url)
 
   t.deepEqual(params, { msg: 'world' })
   t.deepEqual(query, { id: '0' })
