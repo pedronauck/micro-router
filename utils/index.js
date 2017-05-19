93dcf6a9771be45c10564ed25b9773152b7c9728
Pedro Nauck <pedronauck@gmail.com>
2017-05-19 12:35:13 -0300

    chrore: some coding style adjustment

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,11 @@
 const { parse } = require('url')
 const UrlPattern = require('url-pattern')
 
-const getParamsAndQuery = (patternOrRoute, url) => {
+const getParamsAndQuery = (pattern, url) => {
   const { query, pathname } = parse(url, true)
-
-  const route =
-    patternOrRoute instanceof UrlPattern ? patternOrRoute : new UrlPattern(patternOrRoute)
-
+  const route = pattern instanceof UrlPattern ? pattern : new UrlPattern(pattern)
   const params = route.match(pathname)
+
   return { query, params }
 }
 
