300d5e3d096ab430c1021db88221c95eba430937
Pedro Nauck <pedronauck@gmail.com>
2017-10-23 19:13:28 -0200

    feat: add suport for routes with underline and remove default 404 handler

--- utils/index.js
+++ utils/index.js
@@ -1,14 +1,19 @@
 const { parse } = require('url')
 const UrlPattern = require('url-pattern')
 
+const patternOpts = {
+  segmentNameCharset: 'a-zA-Z0-9_-'
+}
+
 const getParamsAndQuery = (pattern, url) => {
   const { query, pathname } = parse(url, true)
-  const route = pattern instanceof UrlPattern ? pattern : new UrlPattern(pattern)
+  const route = pattern instanceof UrlPattern ? pattern : new UrlPattern(pattern, patternOpts)
   const params = route.match(pathname)
 
   return { query, params }
 }
 
 module.exports = {
-  getParamsAndQuery
+  getParamsAndQuery,
+  patternOpts
 }
