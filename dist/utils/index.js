29601efa45d7a7dab94bf7c0ca50058c0c4c652e
Pedro Nauck <pedronauck@gmail.com>
2018-03-06 02:39:23 -0300

    fix: build files

---
+++ dist/utils/index.js
@@ -0,0 +1,22 @@
+const { parse } = require('url');
+const UrlPattern = require('url-pattern');
+
+const patternOpts = {
+  segmentNameCharset: 'a-zA-Z0-9_-'
+};
+
+const isPattern = pattern => pattern instanceof UrlPattern;
+
+const getParamsAndQuery = (pattern, url) => {
+  const { query, pathname } = parse(url, true);
+  const route = isPattern(pattern) ? pattern : new UrlPattern(pattern, patternOpts);
+  const params = route.match(pathname);
+
+  return { query, params };
+};
+
+module.exports = {
+  getParamsAndQuery,
+  isPattern,
+  patternOpts
+};
